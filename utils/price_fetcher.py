9bd6545fc43a4897b7afee4c5c7ca30ad4e5536e
abolfazltr <abolfazlh1374m@gmail.com>
2025-05-19 15:04:52 +0000

    merge get_current_price into price.py and delete price_fetcher

--- utils/price_fetcher.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_current_price(symbol="ETHUSDT"):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-    response = requests.get(url).json()
-    return float(response["price"])
