dbb8e58f48663f28d2f13a371d3f0818a7cf1d1f
abolfazltr <abolfazlh1374m@gmail.com>
2025-05-19 17:35:53 +0000

    added missing ema function

--- utils/indicators.py
+++ utils/indicators.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
+def ema(series, period=14):
+    return series.ewm(span=period, adjust=False).mean()
+
 def supertrend(df, period=10, multiplier=3):
     hl2 = (df['high'] + df['low']) / 2
     atr = df['high'].rolling(window=period).max() - df['low'].rolling(window=period).min()
     atr = atr.rolling(window=period).mean()
+
     upperband = hl2 + (multiplier * atr)
     lowerband = hl2 - (multiplier * atr)
-
     supertrend = [True] * len(df)
 
     for i in range(1, len(df)):
