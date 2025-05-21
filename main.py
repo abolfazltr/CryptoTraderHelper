0d9f1cfadf1918d809d4c7a413570fabb66fddc3
abolfazltr <abolfazlh1374m@gmail.com>
2025-05-21 08:56:36 +0000

    تکمیل پروژه

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import time
 from utils.strategy import get_signal
 from utils.gmx_v2 import open_position
 
-# توکن‌هایی که ربات بررسی می‌کنه
 TOKENS = ["ETH", "LINK"]
 
-# تابع اصلی ربات
 def run_bot():
-    print("✅ ربات تریدر شروع شد...\n")
+    print("✅ ربات تریدر فعال شد...\n")
 
     while True:
         for token in TOKENS:
-            print(f"-----------------------------")
+            print("--------------------------------------")
             print(f"🔍 بررسی توکن: {token}")
-            signal = get_signal(token)
 
-            print(f"📊 سیگنال دریافتی برای {token}: {signal}")
+            signal = get_signal(token)
+            print(f"📊 سیگنال دریافتی: {signal}")
 
             if signal == "buy":
                 print(f"✅ سیگنال خرید برای {token} تأیید شد.")
@@ -24,10 +22,10 @@ def run_bot():
                 print(f"✅ سیگنال فروش برای {token} تأیید شد.")
                 open_position(token_symbol=token, is_long=False)
             else:
-                print(f"❌ هیچ سیگنال معتبری برای {token} صادر نشد.")
+                print(f"❌ هیچ سیگنالی برای {token} صادر نشد.")
 
         print("⏱️ منتظر ۵ دقیقه بعدی...\n")
         time.sleep(300)
 
-# اجرای ربات
+# اجرای خودکار
 run_bot()
